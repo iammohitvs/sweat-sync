ede993d48b2d95ec922c3235c0fb4d456642f2f7
Mohit V S <iammohitvs@Mohits-MacBook-Air.local>
2024-05-04 13:34:09 +0530

    allowed adding a session

--- app/actions.ts
+++ app/actions.ts
@@ -5,30 +5,43 @@ import { range } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 
 export async function addSession(formData: FormData) {
-    /* const wid = formData.get("wid");
+    const wid = formData.get("wid");
 
     const exercises = await supabase
         .from("workouts")
         .select("exercises")
         .eq("id", wid);
 
-    let sessionDetails = new Object;
+    let sessionDetails = [];
 
     Object.entries(exercises.data[0].exercises).map(
         ([exercise, totalSets], index) => {
             range(1, totalSets + 1).map((value) => {
-                sessionDetails = {
+                sessionDetails = [
                     ...sessionDetails,
-                    exercise: [
-                        formData.get(`${exercise}-${value}-weight`),
-                        formData.get(`${exercise}-${value}-weight`),
+                    [
+                        exercise,
+                        [
+                            Number(formData.get(`${exercise}-${value}-weight`)),
+                            Number(formData.get(`${exercise}-${value}-reps`)),
+                        ],
                     ],
-                };
+                ];
             });
         }
     );
 
-    console.log(sessionDetails); */
+    const {data, error} = await supabase
+    .from("sessions")
+    .insert({
+        exercises: sessionDetails,
+        workout_id: wid,
+    })
+    .select()
+
+    if(error) throw new Error("Something went wrong while adding a session")
+
+    console.log(data)
 }
 
 /* export async function addData(formData: FormData) {
